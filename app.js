de35da53c87317ede1170fc076c9528dec7c9f53
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-07 04:06:28 +0530

    Added Query Selectors and Event Listeners

---
+++ app.js
@@ -0,0 +1,11 @@
+const fname = document.querySelector(".fname");
+const lname = document.querySelector(".lname");
+const roll = document.querySelector(".roll");
+const gender = document.querySelector(".gender");
+const blood = document.querySelector(".blood");
+const mobile = document.querySelector(".phone");
+const address = document.querySelector(".address");
+const submitBtn = document.querySelector(".submitBtn");
+const cardContainer = document.querySelector(".card-container");
+
+submitBtn.addEventListener("click", addCard);
